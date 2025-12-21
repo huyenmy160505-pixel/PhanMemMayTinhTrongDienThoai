81a4e67a46c2788bf43b5cf125d17f58bff450ed
huyenmy160505-pixel <huyenmy160505@gmail.com>
2025-12-21 21:41:26 +0700

    Thêm các form giao diện cơ bản
    My đã tạo khái quát các giao diện from

---
+++ frmChuyenDoiDonVi.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace PhanMemMayTinhTrongDienThoai
+{
+    partial class frmChuyenDoiDonVi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // frmChuyenDoiDonVi
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1156, 988);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "frmChuyenDoiDonVi";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Chuyển Đổi Đơn Vị";
+            WindowState = FormWindowState.Maximized;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
