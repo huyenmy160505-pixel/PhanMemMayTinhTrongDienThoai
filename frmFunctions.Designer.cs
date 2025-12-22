019d0409cb1ebf71b21b0883bf1deeca1211bb42
iêng iêng <iengnay.250505@gmail.com>
2025-12-22 12:26:25 +0700

    Làm giao diện của frmFunctions.cs
    Làm giao diện của frmFunctions.cs
    Tạo giao diện dựa vào các yêu  trên tài liệu “Tổng quát” đã đưa
    Tạo kích thức vừa với và chỉnh thông số phù hợp các from yêu cầu
    Thêm buton cho phù hợp rồi đổi text và name cho frmFunctions.cs
    Tiếp frmLichSu.cs, chia giao diện thành 2 và chỉnh sửa bên trái thêm các chức năng phù hợp với yêu cầu

--- frmFunctions.Designer.cs
+++ frmFunctions.Designer.cs
@@ -28,23 +28,259 @@
         /// </summary>
         private void InitializeComponent()
         {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.Controls.Add(button15, 2, 4);
+            tableLayoutPanel1.Controls.Add(button14, 1, 4);
+            tableLayoutPanel1.Controls.Add(button13, 0, 4);
+            tableLayoutPanel1.Controls.Add(button12, 2, 3);
+            tableLayoutPanel1.Controls.Add(button11, 1, 3);
+            tableLayoutPanel1.Controls.Add(button10, 0, 3);
+            tableLayoutPanel1.Controls.Add(button9, 2, 2);
+            tableLayoutPanel1.Controls.Add(button8, 1, 2);
+            tableLayoutPanel1.Controls.Add(button7, 0, 2);
+            tableLayoutPanel1.Controls.Add(button6, 2, 1);
+            tableLayoutPanel1.Controls.Add(button5, 1, 1);
+            tableLayoutPanel1.Controls.Add(button4, 0, 1);
+            tableLayoutPanel1.Controls.Add(button3, 2, 0);
+            tableLayoutPanel1.Controls.Add(button2, 1, 0);
+            tableLayoutPanel1.Controls.Add(button1, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            tableLayoutPanel1.ForeColor = Color.FromArgb(49, 27, 146);
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 5;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.Size = new Size(386, 382);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(243, 229, 245);
+            button1.Dock = DockStyle.Fill;
+            button1.Location = new Point(3, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(122, 70);
+            button1.TabIndex = 0;
+            button1.Text = "↔";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(243, 229, 245);
+            button2.Dock = DockStyle.Fill;
+            button2.Location = new Point(131, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(122, 70);
+            button2.TabIndex = 1;
+            button2.Text = "Rad";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(243, 229, 245);
+            button3.Dock = DockStyle.Fill;
+            button3.Location = new Point(259, 3);
+            button3.Name = "button3";
+            button3.Size = new Size(124, 70);
+            button3.TabIndex = 2;
+            button3.Text = "∛";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(243, 229, 245);
+            button4.Dock = DockStyle.Fill;
+            button4.Location = new Point(3, 79);
+            button4.Name = "button4";
+            button4.Size = new Size(122, 70);
+            button4.TabIndex = 3;
+            button4.Text = "sin⁻¹";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(243, 229, 245);
+            button5.Dock = DockStyle.Fill;
+            button5.Location = new Point(131, 79);
+            button5.Name = "button5";
+            button5.Size = new Size(122, 70);
+            button5.TabIndex = 4;
+            button5.Text = "cos⁻¹";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.FromArgb(243, 229, 245);
+            button6.Dock = DockStyle.Fill;
+            button6.Location = new Point(259, 79);
+            button6.Name = "button6";
+            button6.Size = new Size(124, 70);
+            button6.TabIndex = 5;
+            button6.Text = "tan⁻¹";
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.FromArgb(243, 229, 245);
+            button7.Dock = DockStyle.Fill;
+            button7.Location = new Point(3, 155);
+            button7.Name = "button7";
+            button7.Size = new Size(122, 70);
+            button7.TabIndex = 6;
+            button7.Text = "sinh";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.FromArgb(243, 229, 245);
+            button8.Dock = DockStyle.Fill;
+            button8.Location = new Point(131, 155);
+            button8.Name = "button8";
+            button8.Size = new Size(122, 70);
+            button8.TabIndex = 7;
+            button8.Text = " cosh";
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.FromArgb(243, 229, 245);
+            button9.Dock = DockStyle.Fill;
+            button9.Location = new Point(259, 155);
+            button9.Name = "button9";
+            button9.Size = new Size(124, 70);
+            button9.TabIndex = 8;
+            button9.Text = "tanh";
+            button9.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.FromArgb(243, 229, 245);
+            button10.Dock = DockStyle.Fill;
+            button10.Location = new Point(3, 231);
+            button10.Name = "button10";
+            button10.Size = new Size(122, 70);
+            button10.TabIndex = 9;
+            button10.Text = "sinh⁻¹";
+            button10.UseVisualStyleBackColor = false;
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.FromArgb(243, 229, 245);
+            button11.Dock = DockStyle.Fill;
+            button11.Location = new Point(131, 231);
+            button11.Name = "button11";
+            button11.Size = new Size(122, 70);
+            button11.TabIndex = 10;
+            button11.Text = " cosh⁻¹";
+            button11.UseVisualStyleBackColor = false;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.FromArgb(243, 229, 245);
+            button12.Dock = DockStyle.Fill;
+            button12.Location = new Point(259, 231);
+            button12.Name = "button12";
+            button12.Size = new Size(124, 70);
+            button12.TabIndex = 11;
+            button12.Text = "tanh⁻¹";
+            button12.UseVisualStyleBackColor = false;
+            // 
+            // button13
+            // 
+            button13.BackColor = Color.FromArgb(243, 229, 245);
+            button13.Dock = DockStyle.Fill;
+            button13.Location = new Point(3, 307);
+            button13.Name = "button13";
+            button13.Size = new Size(122, 72);
+            button13.TabIndex = 12;
+            button13.Text = "Text 2ˣ";
+            button13.UseVisualStyleBackColor = false;
+            // 
+            // button14
+            // 
+            button14.BackColor = Color.FromArgb(243, 229, 245);
+            button14.Dock = DockStyle.Fill;
+            button14.Location = new Point(131, 307);
+            button14.Name = "button14";
+            button14.Size = new Size(122, 72);
+            button14.TabIndex = 13;
+            button14.Text = "Text x³";
+            button14.UseVisualStyleBackColor = false;
+            // 
+            // button15
+            // 
+            button15.BackColor = Color.FromArgb(243, 229, 245);
+            button15.Dock = DockStyle.Fill;
+            button15.Location = new Point(259, 307);
+            button15.Name = "button15";
+            button15.Size = new Size(124, 72);
+            button15.TabIndex = 14;
+            button15.Text = "Text x!";
+            button15.UseVisualStyleBackColor = false;
             // 
             // frmFunctions
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(483, 478);
+            ClientSize = new Size(386, 382);
+            Controls.Add(tableLayoutPanel1);
             ForeColor = Color.FromArgb(240, 240, 255);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(2, 2, 2, 2);
             Name = "frmFunctions";
             ShowIcon = false;
             ShowInTaskbar = false;
             StartPosition = FormStartPosition.Manual;
             Text = "frmFunctions";
+            tableLayoutPanel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button button1;
+        private Button button15;
+        private Button button14;
+        private Button button13;
+        private Button button12;
+        private Button button11;
+        private Button button10;
+        private Button button9;
+        private Button button8;
+        private Button button7;
+        private Button button6;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
     }
 }
